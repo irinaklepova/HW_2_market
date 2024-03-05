2fa6ba8e96b3307357d7b9c9297a8145c9536e3d
Irina Klepova <ivklepova23@gmail.com>
2024-03-06 00:17:01 +0400

    настройка URL-файла для нового приложения

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from catalog.views import index, contacts
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('contacts.html', contacts, name='contacts'),
+]
