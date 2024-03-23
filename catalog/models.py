1e38e5e27f341e418ff0418e4887142b84bb52df
Irina Klepova <ivklepova23@gmail.com>
2024-03-23 19:49:13 +0400

    описание моделей Product и Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, verbose_name='Категория')
+    category_description = models.TextField(max_length=500, verbose_name='Описание категории')
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('category_name',)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, verbose_name='Наименование')
+    product_description = models.TextField(max_length=500, verbose_name='Описание продукта')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Стоимость')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.product_name} {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
