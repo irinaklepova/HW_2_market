67b29accbd9a1a0853ef99a8f2531b70a80867e2
Irina Klepova <ivklepova23@gmail.com>
2024-03-24 12:24:25 +0400

    удаление в Product поля manufactured_at, добавление параметра related_name

--- catalog/models.py
+++ catalog/models.py
@@ -20,11 +20,10 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200, verbose_name='Наименование')
     product_description = models.TextField(max_length=500, verbose_name='Описание продукта')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Стоимость')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateField(default='2023-01-01', verbose_name='Дата производства')
 
     def __str__(self):
         return f'{self.product_name} {self.category}'
