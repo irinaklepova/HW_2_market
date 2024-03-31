86e66a147dd5e790852bdf3090bbf7c0f60193d7
Irina Klepova <ivklepova23@gmail.com>
2024-04-01 02:16:47 +0400

    добавлен контроллер для отображения карточки товара, изменен контроллер для отображения главной страницы

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/home.html')
+    object_list = Product.objects.all()
+    context = {
+        'object_list': object_list,
+        'title': 'Носки сделают ваш день!',
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'product': product,
+        'title': 'Информация о товаре',
+    }
+    return render(request, 'catalog/product_info.html', context)
 
 
 def contacts(request):
