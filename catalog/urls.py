e337a135933af84c88f1c7aa1879e2c5ba1b8e5d
Irina Klepova <ivklepova23@gmail.com>
2024-05-01 18:21:11 +0400

    заменены и прописаны новые урлы для контроллеров на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
-from catalog.views import index, contacts, products_list, product_card
+from catalog.views import (IndexView, ContactView, ProductListView, ProductDetailView, ProductCreateView,
+                           ProductUpdateView, ProductDeleteView)
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('products/', products_list, name='products_list'),
-    path('products/<int:pk>/', product_card, name='product_card'),
+    path('', IndexView.as_view(), name='index'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
