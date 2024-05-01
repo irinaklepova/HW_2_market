7f1619f120decaeb7f95773daf19d3f9a7777e25
Irina Klepova <ivklepova23@gmail.com>
2024-05-02 00:34:32 +0400

    добавлена модель Version

--- catalog/models.py
+++ catalog/models.py
@@ -32,3 +32,17 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
         ordering = ('product_name',)
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    version_number = models.PositiveIntegerField(verbose_name='Номер версии')
+    version_name = models.CharField(max_length=150, verbose_name='Название версии', **NULLABLE)
+    is_current = models.BooleanField(default=True, verbose_name='Признак текущей версии')
+
+    def __str__(self):
+        return f'{self.product} {self.version_number} {self.version_name}'
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
