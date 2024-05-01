8d763b481949e19ef5eb38ac4efefab0f24deb15
Irina Klepova <ivklepova23@gmail.com>
2024-05-01 18:27:40 +0400

    описан путь к приложению blog

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = ([
-                  path('admin/', admin.site.urls),
-                  path('', include('catalog.urls', namespace='catalog')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+                   path('admin/', admin.site.urls),
+                   path('', include('catalog.urls', namespace='catalog')),
+                   path('blog/', include('blog.urls', namespace='blog')),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
                static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
