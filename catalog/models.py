7f691af254ae8ba429351185ae07bbf011d02595
Irina Klepova <ivklepova23@gmail.com>
2024-05-07 14:19:51 +0400

    переименованы поля в модели Version

--- catalog/models.py
+++ catalog/models.py
@@ -37,12 +37,12 @@ class Product(models.Model):
 
 class Version(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
-    version_number = models.PositiveIntegerField(verbose_name='Номер версии')
-    version_name = models.CharField(max_length=150, verbose_name='Название версии', **NULLABLE)
+    number = models.PositiveIntegerField(verbose_name='Номер версии')
+    name = models.CharField(max_length=150, verbose_name='Название версии', **NULLABLE)
     is_current = models.BooleanField(default=True, verbose_name='Признак текущей версии')
 
     def __str__(self):
-        return f'{self.product} {self.version_number} {self.version_name}'
+        return f'{self.product} {self.number} {self.name}'
 
     class Meta:
         verbose_name = 'Версия'
