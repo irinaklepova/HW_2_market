c0adace6b3f95c82f1c2f666f06bfaa0c78d134d
Irina Klepova <ivklepova23@gmail.com>
2024-06-02 18:53:08 +0400

    настроено кеширование всего контроллера отображения данных относительно одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.views import (IndexView, ContactView, ProductListView, ProductDetailView, ProductCreateView,
-                           ProductUpdateView, ProductDeleteView)
+                           ProductUpdateView, ProductDeleteView, CategoryListView)
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -10,8 +11,9 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contacts/', ContactView.as_view(), name='contacts'),
     path('products/', ProductListView.as_view(), name='product_list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
 ]
